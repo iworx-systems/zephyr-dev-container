b51fc3ab702ee95f6a1c7ab4a3bc4847a4244756
Scott Laboe <scottyl2.718@gmail.com>
2024-10-07 16:36:32 -0400

    style(init): Functionalize init script; add log  - Refactor init script to use functions so it is easier to understand.   - Output init script to log file.

--- init-devcontainer.sh
+++ init-devcontainer.sh
@@ -1,55 +1,71 @@
 #!/bin/bash
 
-# .west directory won't exist if west isn't initialized
-if [ ! -d .west ]; then
+# redirect output to log
+exec > "/var/log/devcontainer-init.log" 2>&1
+
+init_pre_commits(){
+    # Install pre-commit for all iworx modules that contain
+    # a ".pre-commit-config.yaml" file at its root.
+    num_paths=0
+
+    west list -f {path} | grep iworx | 
+    while read prj_path; do
+        # Check if pre-commit hook already exists
+        if [ ! -s "$PRJ_ROOT_DIR/$prj_path/.git/hooks/pre-commit" ]; then
+            # Check if directory contains a pre-commit config
+            if [ -s "$PRJ_ROOT_DIR/$prj_path/.pre-commit-config.yaml" ]; then
+                if [ $num_paths -eq 0 ]; then
+                    # Print message and separator
+                    echo "--------------------------------------------------"
+                    echo "Setting up pre-commits"
+                fi
+
+                # Setup pre-commit
+                cd "$PRJ_ROOT_DIR/$prj_path"
+                echo "Installing to: $PRJ_ROOT_DIR/$prj_path"
+                pre-commit install
+                
+                # increment the number of paths processed
+                ((num_paths+=1))
+            fi
+        fi
+    done
+
+    if [ ! $num_paths -eq 0 ]; then
+        echo "Pre-commits are setup"
+    fi
+}
+
+init_west(){
     # Make sure app folder exists
-    if [ ! -d app ]; then
-        echo "Unable to init west because there isn't an 'app' folder."
+    if [ ! -d "${APP_DIR}" ]; then
+        echo "Unable to init west because APP_DIR:${APP_DIR} does not exist."
     else
         # Initialize West
-        west init -l app \
+        west init -l "${APP_DIR}" \
         && west config manifest.group-filter -- [+babblesim,+iworx] \
         && west config manifest.project-filter -- +nanopb
 
         # Build net tools if they exist
         if [ -d $NET_TOOLS_BASE ]; then
-            cd $NET_TOOLS_BASE && make
+            cd $NET_TOOLS_BASE && make && cd "${PRJ_ROOT_DIR}"
         fi
     fi
-fi
+}
 
-# Check for updates in manifest
-west update
-
-# Install pre-commit for all iworx modules that contain
-# a ".pre-commit-config.yaml" file at its root.
-num_paths=0
-
-west list -f {path} | grep iworx | 
-while read prj_path; do
-    # Check if pre-commit hook already exists
-    if [ ! -s "$PRJ_ROOT_DIR/$prj_path/.git/hooks/pre-commit" ]; then
-        # Check if directory contains a pre-commit config
-        if [ -s "$PRJ_ROOT_DIR/$prj_path/.pre-commit-config.yaml" ]; then
-            if [ $num_paths -eq 0 ]; then
-                # Print message and separator
-                echo "--------------------------------------------------"
-                echo "Setting up pre-commits"
-            fi
-
-            # Setup pre-commit
-            cd "$PRJ_ROOT_DIR/$prj_path"
-            echo "Installing to: $PRJ_ROOT_DIR/$prj_path"
-            pre-commit install
-            
-            # increment the number of paths processed
-            ((num_paths+=1))
-        fi
+# Check that PRJ_ROOT_DIR exists
+if [ -d "${PRJ_ROOT_DIR}" ]; then
+    cd "${PRJ_ROOT_DIR}"
+    # .west directory won't exist if west isn't initialized
+    if [ ! -d .west ]; then
+        init_west
     fi
-done
+    # Check for updates in manifest
+    west update
 
-if [ ! $num_paths -eq 0 ]; then
-    echo "Pre-commits are setup"
+    init_pre_commits
+else
+    echo "Unable to init devcontainer because PRJ_ROOT_DIR:${PRJ_ROOT_DIR} does not exist"
 fi
 
 # Keep container open after setup
